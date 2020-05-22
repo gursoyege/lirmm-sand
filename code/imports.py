7ed406d888fa3d2c743b6e1694e819ebb6501c87
= <=>
2020-05-22 09:08:39 +0300

    Modified imports

--- code/imports.py
+++ code/imports.py
@@ -1,7 +1,10 @@
-import lxml.etree as ET
+
 import os
 import glob
-from defs import getXml, getXmlNames, writeXml, getAnnNames, nameGen
 import cv2
 import numpy as np
+import lxml.etree as ET
+
 from params import *
+from defs import getXml, getXmlNames, writeXml, getAnnNames, nameGen
+
