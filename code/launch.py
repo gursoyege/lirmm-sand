ea3bed801d6c99c9c88cc91de9d7b760e5cdd521
ege-um <ege.gursoy@etu.umontpellier.fr>
2020-06-05 03:42:20 +0300

    Deleted old code folder

--- code/launch.py
+++
@@ -1,9 +0,0 @@
-from imports import *
-import xml_generator, image_verifier, image_cropper
-
-xml_generator.clean()
-image_cropper.clean()
-
-xml_generator.run()
-image_verifier.run()
-image_cropper.run()
--- code/params.py
+++
@@ -1,7 +0,0 @@
-action = "tap"
-#action = "poke"
-
-ANNOTATION_PATH = action + "/annotations"
-IMAGE_PATH = action + "/images"
-XML_PATH = action + "/xmls"
-DATA_PATH = action + "/data"
